c9563493bed05a6acd89fb93c9b8465ae1ff0aa2
manhnt2310 <nguyenthemanh231003@gmail.com>
2023-03-12 16:21:11 +0700

    [Core] Models - Add Chapter, Section

--- lib/core/models/book.dart
+++ lib/core/models/book.dart
@@ -6,6 +6,7 @@ class Book {
     required this.cover,
     required this.level,
     required this.totalWords,
+    required this.chapters,
   });
 
   final String id;
@@ -13,5 +14,36 @@ class Book {
   final String description;
   final String cover;
   final String level;
+  final int totalWords;
+  final List<Chapter> chapters;
+}
+
+class Chapter {
+  const Chapter({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.totalWords,
+    required this.sections,
+  });
+
+  final int id;
+  final String title;
+  final String description;
+  final int totalWords;
+  final List<Section> sections;
+}
+
+class Section {
+  const Section({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.totalWords,
+  });
+
+  final int id;
+  final String title;
+  final String description;
   final int totalWords;
 }
