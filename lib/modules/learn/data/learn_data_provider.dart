7569417a2ad15d995a1b95c023f00f75319796c9
manhnt2310 <nguyenthemanh231003@gmail.com>
2024-04-14 16:18:29 +0700

    [Learn] Fix bug read data from file json

--- lib/modules/learn/data/learn_data_provider.dart
+++ lib/modules/learn/data/learn_data_provider.dart
@@ -4,21 +4,11 @@ import 'package:ll/core/models/word.dart';
 
 class LearnDataProvider {
   Future<List<Word>> getWordList() async {
-    // final String response = await rootBundle.loadString(
-    //   'lib/assets/datasource/words.json',
-    // );
-    // final data = await json.decode(response);
-    // return data.map((e) => Word.fromJson(e)).toList();
-    return [
-      const Word(
-        id: 1,
-        bookId: 1,
-        chapterId: 1,
-        sectionId: 1,
-        label: 'doctor',
-        translatedLabel: 'bac si',
-        examples: [], //'what is a doctor?, How many doctor?',
-      )
-    ];
+    final String response = await rootBundle.loadString(
+      'lib/assets/datasource/words.json',
+    );
+    final data = await json.decode(response);
+    final words = Word.listFromJsons(data);
+    return words;
   }
 }
